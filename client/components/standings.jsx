11b07579bf983602b05677091006e16c22ea77c7
Talal <nweirant@gmail.com>
2019-04-30 11:54:24 -0400

    converted to standings to tables

--- client/components/standings.jsx
+++ client/components/standings.jsx
@@ -73,8 +73,118 @@ export default class Standings extends React.Component {
  
     render() {
         return(
-            <div>
-                
+            <div className="row">
+                <div>
+                    <h4>AL EAST</h4>
+                    <table>
+                    <tr>
+                        <th>Team</th>
+                        <th>W</th>
+                        <th>L</th>
+                    </tr>
+                        {this.state.al_east.map(team => (     
+                            <tr>
+                                <td>{team.Tm}</td>
+                                <td>{team.W}</td>
+                                <td>{team.L}</td>
+                            </tr>
+
+                        ))}
+                     </table>
+                </div>
+
+                <div>
+                    <h4>AL WEST</h4>
+                    <table>
+                    <tr>
+                        <th>Team</th>
+                        <th>W</th>
+                        <th>L</th>
+                    </tr>
+                        {this.state.al_west.map(team => (     
+                            <tr>
+                                <td>{team.Tm}</td>
+                                <td>{team.W}</td>
+                                <td>{team.L}</td>
+                            </tr>
+
+                        ))}
+                     </table>
+
+                </div>
+
+                <div>
+                    <h4>AL CENTRAL</h4>
+                    <table>
+                    <tr>
+                        <th>Team</th>
+                        <th>W</th>
+                        <th>L</th>
+                    </tr>
+                        {this.state.al_central.map(team => (     
+                            <tr>
+                                <td>{team.Tm  }</td>
+                                <td>{team.W}</td>
+                                <td>{team.L}</td>
+                            </tr>
+
+                        ))}
+                     </table>
+                </div>
+                <div>
+                    <h4>NL EAST</h4>
+                    <table>
+                    <tr>
+                        <th>Team</th>
+                        <th>W</th>
+                        <th>L</th>
+                    </tr>
+                        {this.state.nl_east.map(team => (     
+                            <tr>
+                                <td>{team.Tm  }</td>
+                                <td>{team.W}</td>
+                                <td>{team.L}</td>
+                            </tr>
+
+                        ))}
+                     </table>
+                </div>
+                <div>
+                    <h4>NL WEST</h4>
+                    <table>
+                    <tr>
+                        <th>Team</th>
+                        <th>W</th>
+                        <th>L</th>
+                    </tr>
+                        {this.state.nl_west.map(team => (     
+                            <tr>
+                                <td>{team.Tm  }</td>
+                                <td>{team.W}</td>
+                                <td>{team.L}</td>
+                            </tr>
+
+                        ))}
+                     </table>
+                </div>
+                <div>
+                    <h4>NL CENTRAL</h4>
+                    <table>
+                    <tr>
+                        <th>Team</th>
+                        <th>W</th>
+                        <th>L</th>
+                    </tr>
+                        {this.state.nl_central.map(team => (     
+                            <tr>
+                                <td>{team.Tm  }</td>
+                                <td>{team.W}</td>
+                                <td>{team.L}</td>
+                            </tr>
+
+                        ))}
+                     </table>
+                </div>
             </div>
         )
     }
